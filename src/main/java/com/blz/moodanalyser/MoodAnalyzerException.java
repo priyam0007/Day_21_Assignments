3d4a9f37355bf8e24538ae17d8617ebb10afb0b2
Priya <Priyamadiwalar0007@gmail.com>
2022-03-13 15:30:43 +0530

    Added:moodanalyser

---
+++ src/main/java/com/blz/moodanalyser/MoodAnalyzerException.java
@@ -0,0 +1,7 @@
+package com.blz.moodanalyser;
+
+public class MoodAnalyzerException extends Exception {
+	public MoodAnalyzerException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/test/java/com/blz/moodanalyser/MoodAnalyzerTest.java
@@ -0,0 +1,55 @@
+package com.blz.moodanalyser;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyzeMood();
+		} catch (MoodAnalyzerException e) {
+			System.out.println(e);
+		}
+		assertEquals("Sad", mood);
+	}
+
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyzeMood();
+			assertEquals("Happy", mood);
+		} catch (MoodAnalyzerException e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	public void givenNullMood_ShouldReturnHappy() {
+		String mood = null;
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+		try {
+			mood = moodAnalyzer.analyzeMood();
+			assertEquals("Happy", mood);
+		} catch (MoodAnalyzerException e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	public void givenEmptyMood_ShouldReturnHappy() {
+		String mood = null;
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		try {
+			mood = moodAnalyzer.analyzeMood();
+			assertEquals("Happy", mood);
+		} catch (MoodAnalyzerException e) {
+			System.out.println(e);
+		}
+	}
+}
